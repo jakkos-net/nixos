d6d59f342d296d75ae347fd9a957874399deebe6
jakkos-net <45759112+jakkos-net@users.noreply.github.com>
2023-12-10 14:40:02 +0000

    .

--- configuration.nix
+++ configuration.nix
@@ -33,21 +33,7 @@
   # locale
   time.timeZone = "Europe/London";
   i18n.defaultLocale = "en_GB.UTF-8";
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "en_GB.UTF-8";
-    LC_IDENTIFICATION = "en_GB.UTF-8";
-    LC_MEASUREMENT = "en_GB.UTF-8";
-    LC_MONETARY = "en_GB.UTF-8";
-    LC_NAME = "en_GB.UTF-8";
-    LC_NUMERIC = "en_GB.UTF-8";
-    LC_PAPER = "en_GB.UTF-8";
-    LC_TELEPHONE = "en_GB.UTF-8";
-    LC_TIME = "en_GB.UTF-8";
-  };
-  services.xserver = {
-    layout = "gb";
-    xkbVariant = "";
-  };
+  services.xserver.layout = "gb";
   console.keyMap = "uk";  
 
   # desktop
@@ -80,12 +66,11 @@
 
   home-manager = {
     extraSpecialArgs = { inherit inputs outputs; };
-    home.stateVersion = "23.05"; # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
-
     users.jak = {
       home = {
         username = "jak";
         homeDirectory = "/home/jak";
+        stateVersion = "23.05"; # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
       };
 
       # installed programs

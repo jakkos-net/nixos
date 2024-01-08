557c191f288957b09c94073919c6589c794fb72e
jakkos-net <45759112+jakkos-net@users.noreply.github.com>
2024-01-08 13:23:14 +0000

    firefox shortcut

--- dot/desktop_entries.nix
+++ dot/desktop_entries.nix
@@ -3,6 +3,7 @@ let
   # for each item in this list, create a desktop entry
   # e.g. this allows me start a firefox private window from my app launcher
   entries = [
+    {name = "ff"; exec = "firefox --new-window";}
     {name = "fp"; exec = "firefox --private-window";}
     {name = "ym"; exec = "firefox --new-window music.youtube.com";}
     {name = "wa"; exec = "firefox --new-window web.whatsapp.com";}

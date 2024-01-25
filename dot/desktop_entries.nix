8ee4f198c11365fb00e36a968de046ad983ead1f
jakkos-net <45759112+jakkos-net@users.noreply.github.com>
2024-01-25 15:02:39 +0000

    helper function for opening multiple browser tabs at once

--- dot/desktop_entries.nix
+++ dot/desktop_entries.nix
@@ -1,14 +1,19 @@
 {lib, ...} :
 let
+  browser = tabs : "firefox --new-window " + (builtins.concatStringsSep " "(builtins.map (tab : "--new-tab -url ${tab}") tabs));
   # for each item in this list, create a desktop entry
   # e.g. this allows me start a firefox private window from my app launcher
   entries = [
-    {name = "ff"; exec = "firefox --new-window";}
+    {name = "ff"; exec = browser [];}
     {name = "fp"; exec = "firefox --private-window";}
-    {name = "ym"; exec = "firefox --new-window music.youtube.com";}
-    {name = "wa"; exec = "firefox --new-window web.whatsapp.com";}
-    {name = "gm"; exec = "firefox --new-window gmail.com";}
-    {name = "ta"; exec = "firefox --new-window https://calendar.google.com/calendar/u/0/r/tasks";}
+    {name = "wa"; exec = browser [
+      "https://calendar.google.com/calendar/u/0/r/tasks"
+      "https://calendar.google.com/calendar/u/0/r"
+    ];}
+    {name = "ta"; exec = browser [
+      "https://calendar.google.com/calendar/u/0/r/tasks"
+      "https://calendar.google.com/calendar/u/0/r"
+    ];}
     {name = "rustdesk"; exec = "rustdesk";} # for some reason nix pkg doesn't come with desktop entry?
   ];
 in

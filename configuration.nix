73badb139c57d6ff807371091fba5a182cb4bc26
jakkos-net <45759112+jakkos-net@users.noreply.github.com>
2024-04-12 12:12:38 +0100

    update flake and rename option

--- configuration.nix
+++ configuration.nix
@@ -28,8 +28,8 @@
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
   services.xserver.enable = true;
-  services.xserver.displayManager.autoLogin.enable = true;
-  services.xserver.displayManager.autoLogin.user = "jak";
+  services.displayManager.autoLogin.enable = true;
+  services.displayManager.autoLogin.user = "jak";
 
   # sound
   sound.enable = true;
